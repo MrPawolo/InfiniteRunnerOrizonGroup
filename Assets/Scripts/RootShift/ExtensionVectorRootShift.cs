6567909ef5db921a1196a8d6921b146d1e491e8f
MrPawolo <77783481+MrPawolo@users.noreply.github.com>
2023-04-30 21:47:33 +0200

    RootShift

---
+++ Assets/Scripts/RootShift/ExtensionVectorRootShift.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace ML.RootShift
+{
+    public static class ExtensionVectorRootShift
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 RealToShifted(this Vector3 position)
+        {
+            return RootShiftManager.RealToShifted(position);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 ShiftedToReal(this Vector3 position)
+        {
+            return RootShiftManager.ShiftedToReal(position);
+        }
+    }
+}
---
+++ Assets/Scripts/RootShift/RootShiftManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace ML.RootShift
+{
+    public class RootShiftManager : MonoBehaviour
+    {
+        [SerializeField, Min(0)] float moveDistance = 100f;
+        [SerializeField] Transform rootTransform;
+        [SerializeField] Transform rootReference;
+
+        public static RootShiftManager Instance { get; private set; }
+        public static Vector3 ShiftAmount { get; private set; }
+        public static Transform Root { get; private set; }
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                transform.SetParent(null);
+                Instance = this;
+                DontDestroyOnLoad(this);
+                Root = rootTransform;
+            }
+            else
+            {
+                Destroy(this);
+            }
+
+            if(rootReference == null)
+            {
+                rootReference = Camera.main.transform;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if(Instance == this)
+            {
+                ShiftAmount = Vector3.zero;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (ShouldShift())
+            {
+
+                Vector3 projectedRootDifference = new Vector3(
+                    rootReference.position.x,
+                    0,
+                    rootReference.position.z);
+                ShiftAmount -= projectedRootDifference;
+                print(ShiftAmount);
+                rootTransform.position = ShiftAmount;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        bool ShouldShift()
+        {
+            Vector2 projectedReference = new Vector2(rootReference.position.x, rootReference.position.z);
+            float distanceToRoot = projectedReference.magnitude;
+            return distanceToRoot > moveDistance;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 RealToShifted(Vector3 position)
+        {
+            return position + ShiftAmount;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 ShiftedToReal(Vector3 position)
+        {
+            return position - ShiftAmount;
+        }
+    }
+}
